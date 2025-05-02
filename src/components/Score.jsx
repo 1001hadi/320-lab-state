00fd1b6352bcefb20e4b3eac75d3ddd15d535f1c
hadi.jaf <hadi.jaf.1001@gmail.com>
2025-05-02 14:20:09 -0600

    bug fixed in score.jsx, same css added

--- src/components/Score.jsx
+++ src/components/Score.jsx
@@ -2,7 +2,7 @@ export const Score = ({ scoreInfo }) => {
   // display learner is Score data on p tag
   return (
     <>
-      <p>Data: {scoreInfo.data}</p>
+      <p>Data: {scoreInfo.date}</p>
       <p>Score: {scoreInfo.score}</p>
     </>
   );
